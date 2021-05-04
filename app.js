ea1a7ef40d847446fb81b2fbd1ea444bcb7761f6
Ata-92 <atakal92@gmail.com>
2021-05-04 22:04:50 +0300

    Add variables

---
+++ app.js
@@ -0,0 +1,9 @@
+class Triangle {
+  valid() {
+    const number1 = prompt("Enter the first number : ");
+    const number2 = prompt("Enter the second number : ");
+    const number3 = prompt("Enter the third number : ");
+    const numbers = Array.from(number1, number2, number3);
+    // if (numbers.map((i, index) => i > 0 && i <= numbers[index]))
+  }
+}
