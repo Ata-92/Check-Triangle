fcf71e66146ab4e0ecd4acb0a9c957117b03b6aa
Ata-92 <atakal92@gmail.com>
2021-05-07 23:48:38 +0300

    Add number validation method

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ class Triangle {
     this.side2 = side2;
     this.side3 = side3;
   }
+  valid(numbers) {
+    if (numbers.some(i => i <= 0) || ((numbers.sort()[2] * 2) > numbers.reduce((acc, curr) => parseFloat(acc) + parseFloat(curr)))) alert("Invalid Triangle");
+    else if (numbers.every(i => !isNaN(i))) this.triangle(numbers);
+  }
 
 }
 
